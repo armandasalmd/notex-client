54b9bca5f784c086bad153df775d918324be0670
Armandas Barkauskas (barkausa) <barkausa@coventry.ac.uk>
2021-06-18 17:40:28 +0100

    Display web title in reading page

--- src/components/pages/shared/ArticlePage/ArticleHeader/ArticleHeader.jsx
+++ src/components/pages/shared/ArticlePage/ArticleHeader/ArticleHeader.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import { Constants } from "@utils";
 
 import "./ArticleHeader.less";
 import ArticleDetails from "./ArticleDetails";
 import HeaderToolbar from "./HeaderToolbar";
+import { Title } from "##/HeadTitle";
 
 export const ArticleHeader = () => {
+    const title = useSelector((state) => state.reading.header.title);
+
     return (
         <div className="articlePage__header">
+            {
+                title && <Title title={`${Constants.appName} - ${title}`} />
+            }
             <ArticleDetails />
             <HeaderToolbar />
         </div>
